fb92a8a7c0ec2162dedde41cd82862f7d0694cc3
arthurfiorette <arthur.fiorette@gmail.com>
2022-02-28 14:06:56 -0300

    refactor(object-code): optimized object-code

--- packages/object-code/src/index.ts
+++ packages/object-code/src/index.ts
@@ -5,49 +5,42 @@
  *
  * @param property The property to Serialize
  * @returns A unique string representation of the property
- * @implNote - As symbols are intended to be unique, an random number is added at the end of the stringified symbol.
- *  - Dates are converted using the `Date#getTime` function
- *  - Objects or arrays will have a `!` prefix to prevent conflict with literal values.
- *  - Objects will be sorted before being converted to a string, so that:
- *
- * ```js
- * serialize({ a: 1, b: 2 }) === serialize({ b: 2, a: 1 })
- * ```
  */
 export function serialize<T>(value?: T): string {
   const type = typeof value;
 
-  if (value && type === 'object' && !(value instanceof Date || value instanceof RegExp)) {
-    const copy = (Array.isArray(value) ? [] : {}) as Record<keyof T, string>;
-
-    for (const key in value) {
-      copy[key as keyof T] = serialize(value[key]);
-    }
-
-    return `${
-      //@ts-expect-error ignore if not present
-      value.constructor
-    }${JSON.stringify(copy, Object.keys(value).sort())}`;
+  if (!value || type !== 'object' || value instanceof Date || value instanceof RegExp) {
+    return `${type}${String(value)}`;
   }
 
-  return `${type}${String(value)}`;
+  const copy = (Array.isArray(value) ? [] : {}) as Record<keyof T, string>;
+
+  for (const key in value) {
+    copy[key] = serialize(value[key]);
+  }
+
+  return `${
+    //@ts-expect-error ignore if not present
+    value.constructor
+  }${JSON.stringify(copy, Object.keys(value).sort())}`;
 }
 
 /**
- * Hashes an value into a signed integer.
+ * Hashes an value into a signed integer. Inspired from `string-hash`.
  *
  * @param serialized The value to be hashed
  * @returns The hashed number value
- *
- *   Inspired from [string-hash](https://www.npmjs.com/package/string-hash) package
+ * @see https://www.npmjs.com/package/string-hash
  */
 export function hashCode(val?: unknown): number {
   val = serialize(val);
 
   let hash = 5381;
 
-  for (let i = 0; i < (val as string).length; i++) {
-    hash = (hash * 33) ^ (val as string).charCodeAt(i);
+  //@ts-expect-error faster for in inside strings
+  // eslint-disable-next-line @typescript-eslint/no-for-in-array
+  for (const index in val as string) {
+    hash = (hash * 33) ^ (val as string).charCodeAt(index as unknown as number);
   }
 
   // Unsign the number
