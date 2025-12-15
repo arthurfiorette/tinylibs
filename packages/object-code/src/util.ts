7d5506fb2f3b7d0d7ba7844f9c0a008b4fdaefaa
Arthur Fiorette <me@arthur.place>
2025-12-15 18:57:44 -0300

    code

--- packages/object-code/src/util.ts
+++ packages/object-code/src/util.ts
@@ -5,7 +5,3 @@ export function sortNumbers(a: string, b: string) {
 export function sortPairsByKey(a: [unknown, unknown], b: [unknown, unknown]) {
   return sortNumbers(String(a[0]), String(b[0]));
 }
-
-export function pairFromKey(val: any, key: string): [unknown, unknown] {
-  return [key, val[key as keyof typeof val]] as [unknown, unknown];
-}
